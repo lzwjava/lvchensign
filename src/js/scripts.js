927bccd4d505212644feb5144584ea146f9b2fb9
lzwjava <lzwjava@gmail.com>
2020-04-26 03:24:37 +0800

    add jpg

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -40,10 +40,24 @@
   $(window).scroll(navbarCollapse);
 
   // Magnific popup calls
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
+  // $('#portfolio').magnificPopup({
+  //   delegate: 'a',
+  //   type: 'image',
+  //   tLoading: '加载中 #%curr%...',
+  //   mainClass: 'mfp-img-mobile',
+  //   gallery: {
+  //     enabled: true,
+  //     navigateByImgClick: true,
+  //     preload: [0, 1]
+  //   },
+  //   image: {
+  //     tError: '<a href="%url%"> 图片 #%curr%</a> 加载失败。'
+  //   }
+  // });
+
+  var basicOptions = {
     type: 'image',
-    tLoading: 'Loading image #%curr%...',
+    tLoading: '加载中 #%curr%...',
     mainClass: 'mfp-img-mobile',
     gallery: {
       enabled: true,
@@ -51,8 +65,111 @@
       preload: [0, 1]
     },
     image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+      tError: '<a href="%url%"> 图片 #%curr%</a> 加载失败。'
     }
-  });
+  };
+
+  var clients = [
+    {
+      name: 'zhoushengsheng',
+      picsNum: 7
+    },
+    {
+      name: 'shiye',
+      picsNum: 4
+    },    
+    {
+      name: 'chaoshan',
+      picsNum: 2,
+    },
+    {
+      name: 'dinglong',
+      picsNum: 5
+    },
+    {
+      name: 'gxg.kids',
+      picsNum: 2
+    },
+    {
+      name: 'qianwei',
+      picsNum: 2
+    },
+    {
+      name: 'salon',
+      picsNum: 4
+    },
+    {
+      name: 'taier',
+      picsNum: 2
+    },
+    {
+      name: 'taixing',
+      picsNum: 4
+    },
+    {
+      name: 'xingyihui',
+      picsNum: 2
+    },
+    {
+      name: 'zhuchongyun',
+      picsNum: 2
+    },
+    {
+      name: 'gac',
+      picsNum: 4
+    },
+    {
+      name: 'rules',
+      picsNum: 5
+    },
+    {
+      name: 'shanghai',
+      picsNum: 3
+    },
+    {
+      name: 'wangxiang',
+      picsNum: 1
+    },
+    {
+      name: 'gongfu',
+      picsNum: 1
+    },
+    {
+      name: 'mc.choice',
+      picsNum: 1
+    },
+    {
+      name: 'mingjiang',
+      picsNum: 1
+    }
+  ];
+
+  for(var i = 0; i < clients.length; i++) {
+
+    var items = [];
+
+    for (var j = 0; j < clients[i].picsNum; j++) {
+      items.push({
+        src: 'assets/img/portfolio/' + clients[i].name + '/' + (j+1) + '.jpg'
+      });
+    }
+
+    // console.log(items);
+
+    $('#portfolio' + (i + 1)).magnificPopup({
+      items: items,
+      type: 'image',
+      tLoading: '加载中 #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+      },
+      image: {
+        tError: '<a href="%url%"> 图片 #%curr%</a> 加载失败。'
+      }
+    });
+  }
 
 })(jQuery); // End of use strict
